cea97af3273aaa3356f6024220d98cad9638e959
David Pedoneze <dpedoneze@caiena.net>
2017-06-24 13:04:30 -0300

    auto resize video

--- assets/javascripts/video-modal.js
+++ assets/javascripts/video-modal.js
@@ -1,12 +1,34 @@
 $(function(){
   'use strict';
 
-  var modal = $('#modal'),
-      modalButton = $('[data-video]');
 
-  modalButton.on('click', function(){
-    modal.modal('toggle');
+
+
+  // var modal = $('#modal'),
+  //     modalButton = $('[data-video]');
+
+  // modalButton.on('click', function(){
+  //   modal.modal('toggle');
+  // });
+
+  videojs("my-video").ready(function(){
+    resizeVideo();
   });
 
 
+  window.addEventListener('resize', function(event){
+    resizeVideo();
+  });
+
+  function resizeVideo() {
+    var video = $('.vjs-tech'),
+        windowSize = $(window).width(),
+        currentLeft = (2541 - windowSize) / 2;
+
+    if (windowSize <= 767) {
+      currentLeft = 0;
+    }
+
+    $('.video-left').css({ left: '-' + currentLeft + 'px' });
+  }
 });
